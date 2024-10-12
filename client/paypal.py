a216438eb1a47d6444aeb49d818283f9f4797992
Brapido <129673180+Brapido@users.noreply.github.com>
2024-10-12 12:57:10 -0400

    Add files via upload
    Commit -1

---
+++ client/paypal.py
@@ -0,0 +1,102 @@
+import requests # type: ignore
+import json
+
+from .models import Subscription
+
+from django.conf import settings
+
+def get_access_token():
+
+    data = {'grant_type': 'client_credentials'}
+    headers = {'Accept': 'application/json', 'Accept-Language': 'en_US'}
+    client_id = settings.SS_CLIENT_ID
+    secret_id = settings.SS_SECRET_ID
+    # For live application delete 'sandbox' and replace client_id & secret_id for live variables
+    url = 'https://api.sandbox.paypal.com/v1/oauth2/token'
+    r = requests.post(url, auth=(client_id, secret_id), headers=headers, data=data).json()
+    access_token = r['access_token']
+    return access_token
+
+def cancel_subscription_paypal(access_token, subID):
+
+    bearer_token = 'Bearer ' + access_token
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': bearer_token,
+    }
+
+    # For live application delete 'sandbox' and replace client_id & secret_id for live variables
+    url = 'https://api.sandbox.paypal.com/v1/billing/subscriptions/' + subID + '/cancel'
+    r = requests.post(url, headers=headers)
+    print(r.status_code)
+
+def update_subscription_paypal(access_token, subID):
+
+    bearer_token = 'Bearer ' + access_token
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': bearer_token,
+    }
+
+    # obtain current subscription plan for the user
+    subDetails = Subscription.objects.get(paypal_subscription_id=subID)
+    current_sub_plan = subDetails.subscription_plan
+    if current_sub_plan == "Standard":
+        new_sub_plan_id= 'P-4HF936665H352344KM35OBIY' # To Premium
+    elif current_sub_plan == "Premium":
+        new_sub_plan_id = 'P-6B3737358P965530NM4C46SA' # To Standard
+
+    # For live application delete 'sandbox' and replace client_id & secret_id for live variables
+    url = 'https://api.sandbox.paypal.com/v1/billing/subscriptions/' + subID + '/revise'
+
+    revision_data = {
+        "plan_id": new_sub_plan_id
+    }
+
+    # Make a POST request to PayPal's API for updating/revising a subscription
+    r = requests.post(url, headers=headers, data=json.dumps(revision_data))
+
+    # Output the response from PayPal to console.
+    response_data = r.json()
+    print(response_data)
+
+    # loop through response data from Paypal to grab HATEOAS approval link
+    for link in response_data.get('links', []):
+        if link.get('rel') == 'approve':
+            approve_link = link['href']
+
+    # checking that original POST request was valid
+    if r.status_code == 200:
+        print("Request was a success")
+        return approve_link
+    else:
+        print("Sorry, an error occured!")
+
+def get_current_subscription(access_token, subID):
+
+    bearer_token = 'Bearer ' + access_token
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': bearer_token,
+    }
+
+    # For live application delete 'sandbox' and replace client_id & secret_id for live variables
+    url = f'https://api.sandbox.paypal.com/v1/billing/subscriptions/{subID}'
+    r = requests.get(url, headers=headers)
+    if r.status_code == 200:
+        subscription_data = r.json()
+        current_plan_id = subscription_data.get('plan_id')
+        return current_plan_id
+    else:
+        print("Failed to retreive subsciption details")
+        return None
+
+    
+    
+
+
+    
+    
+
+
+
---
+++ sub_scriber/settings.py
@@ -0,0 +1,185 @@
+# Import Django environ and initialize environment variables
+import environ # type: ignore
+
+env = environ.Env()
+
+environ.Env.read_env()
+
+
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+# CSRF_TRUSTED_ORIGINS = [] 
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'crispy_forms',
+    'crispy_bootstrap5',
+
+    'account',
+    'writer',
+    'client',
+
+    # AWS S3 integration app
+    'storages',
+]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+# allow pop-ups from paypal
+SECURE_CROSS_ORIGIN_OPENER_POLICY='same-origin-allow-popups'
+
+AUTH_USER_MODEL = 'account.CustomUser'
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'sub_scriber.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'sub_scriber.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
+'''
+# SQLite database (development - default)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+'''
+# PostgreSQL database (production)
+import dj_database_url #type: ignore
+
+DATABASES = {
+
+    'default': dj_database_url.parse(env('DATABASE_URL'))
+
+}
+
+# Password validation
+# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/5.1/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.1/howto/static-files/
+
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# SMTP Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+# EMAIL_USE_SSL = True
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
+
+# Amazon S3 Configuration
+AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID') # - Enter your AWS Access Key ID HERE
+AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY') # - Enter your AWS Secret Access Key ID HERE
+AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME') # - Enter your S3 bucket name HERE
+
+STORAGES = {
+
+    # Media file (image) management
+    "default": {
+        "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
+    },
+    
+    # CSS and JS file management
+    "staticfiles": {
+        "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
+        
+    },
+}
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_FILE_OVERWRITE = False
+
+# PayPal credentials
+SS_CLIENT_ID = env('SS_CLIENT_ID')
+SS_SECRET_ID = env('SS_SECRET_ID')
